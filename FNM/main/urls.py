7522e0732d8a51020c38d57de8a2507228c71493
Shcherbyna-Nazar <nazar.force@gmail.com>
2022-02-15 11:58:57 +0100

    muzyka gromcze glaza zakryty

--- FNM/main/urls.py
+++ FNM/main/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns = [
 
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('tank1', views.tank1, name='tank1')
 ]
--- FNM/main/views.py
+++ FNM/main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def tank1(request):
+    return render(request, '0001.html')
