f8aa293e9c8405b5d06ea78cd7976e2b9eea5698
Shcherbyna-Nazar <nazar.force@gmail.com>
2022-02-16 17:43:04 +0100

    dsadas

--- FNM/main/models.py
+++ FNM/main/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class ProductModel(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    category = models.CharField(max_lenght=100)
+    category = models.CharField(max_пшеlenght=100)
     price = models.CharField(max_lenght=100)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
