7e1b4f043e9542406b40472a5b2871f8193f8166
Philip Sorokin <97840410+philip-sor@users.noreply.github.com>
2022-02-16 14:24:39 +0200

    added products to context dictinary

--- FNM/main/views.py
+++ FNM/main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+from .models import ProductModel
+form django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    products = ProductModel.objects.all()
+    context = {'products': products}
+    return render(request, 'index.html', context)
 
 
 def tank1(request):
     context = {}
-    return render(request, '0001.html', context=context)
+    return render(request, '0001.html', context)
