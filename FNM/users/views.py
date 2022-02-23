60b4a7775e5bd178c56c29e1573e5389e4b9ebe0
Philip Sorokin <97840410+philip-sor@users.noreply.github.com>
2022-02-23 19:17:30 +0200

    Update views.py

--- FNM/users/views.py
+++ FNM/users/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
-from .forms import UserForm
+from .forms import UserLoginForm, UserRegistrationForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def register_user:
-  pass
-
+  form = UserRegistrationForm
+  if request.method == 'POST':
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+      new_user = form.save(commit=False)
+      new_user.save()
+      return redirect('home')
+    else:
+      messages.error(request, 'SUCK SOME COCK U STUPID BITCH!!!')
+    
+  context = {'form': form}
+  return render(request, "login_register.html", context)
 
 def login_user:
-  pass
+  form = UserLoginForm
+  if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'SUCK SOME COCK U STUPID BITCH!!!')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'SUCK SOME COCK U STUPID BITCH!!!')
+  context = {'form': form}
+  return render(request, "login_register.html", context)
+
